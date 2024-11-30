650ec1038b3c7e7e47c20ad761f4dfe5a447cdbe
Safouan El Khammari <sael-kha@c1r2p2.1337.ma>
2024-11-30 18:17:33 +0100

    fuck memory safety

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sael-kha <sael-kha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 12:34:45 by sael-kha          #+#    #+#             */
+/*   Updated: 2024/11/20 13:31:48 by sael-kha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		gnl_strlen(char *s1);
+char	*get_next_line(int fd);
+char	*strjoin(char *s1, char *s2);
+int		is_on_str(char *str, char c);
+char	*check(void);
+char	*gbed_line(char *text);
+char	*update_text(char *text);
+char	*gbed_text(int fd, char *text);
+
+#endif
